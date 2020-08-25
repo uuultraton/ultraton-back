9201fb2ae646853117fb001dd0ed74e171c76d0b
ohexus <moskalenko.taras.d@gmail.com>
2020-08-25 22:46:46 +0300

    fixed auth validators regexps

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -6,22 +6,20 @@ module.exports = {
 
     lastName: Joi.string().min(3).max(30).required(),
 
-    email: Joi.string()
-      .pattern(new RegExp('^([a-z0-9_.-]+)@([a-z0-9_.-]+).([a-z.]{2,6})$'))
-      .required(),
+    email: Joi.string().email().required(),
 
     password: Joi.string()
       .min(3)
-      .pattern(new RegExp('^[a-zA-Z0-9!@#$%&*]{3,25}$'))
+      .pattern(new RegExp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$'))
       .required(),
   }),
 
   login: Joi.object({
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
 
     password: Joi.string()
       .min(3)
-      .pattern(new RegExp('^[a-zA-Z0-9!@#$%&*]{3,25}$'))
+      .pattern(new RegExp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$'))
       .required(),
   }),
 };
