5b80dfcda303aa2d7adec6fe4512143b1edda5c1
ohexus <moskalenko.taras.d@gmail.com>
2020-08-26 13:51:07 +0300

    added ability to update users skills

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,11 +8,15 @@ const { userValid } = require('../validators');
 
 const {
   getUser,
+  getSkills,
   updateAvatar,
   updateAbout,
 } = require('../controllers/user.controller');
 
 router.get('/:id', (req, res) => getUser(req, res));
+
+router.get('/:id/skills', (req, res) => getSkills(req, res));
+router.get('/:id/updateSkills', (req, res) => updateSkills(req, res));
 
 router.patch('/:id/updateAbout', valid(userValid.aboutUpload), (req, res) =>
   updateAbout(req, res),
